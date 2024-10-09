f26987dbb8f1c44fe2fc53f26681df2f429784c3
abdelrahman027 <abdelrahmansaad027@gmail.com>
2024-10-09 14:29:30 +0300

    carousl remake

--- components/DestinationsCarousel.jsx
+++ components/DestinationsCarousel.jsx
@@ -1,10 +1,16 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const DestinationsCarousel = () => {
-
   const [destinations, setDestinations] = useState([]);
 
   useEffect(() => {
@@ -15,51 +21,39 @@ const DestinationsCarousel = () => {
       });
   }, []);
   return (
-    <div className="container flex flex-col justify-center items-center gap-y-4 my-4">
-      <div className="carousel h-[50vh]">
-        {destinations.map((destination, index) => (
-          <div
-            key={destination.id}
-            id={`slide${index}`}
-            className="carousel-item relative w-full"
-          >
-            <div className="absolute w-1/4 top-8 left-16 z-10 text-white p-20 bg-dark/75">
-              <div>{destination.city + ' , ' + destination.country}</div>
-              <div>{destination.continent}</div>
-            </div>
-            <img
-              src={destination.image}
-              alt={destination.Name}
-              className="w-full object-cover"
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={10}
+      slidesPerView={2}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      className='relative'
+    >
+  
+      {destinations.map(dest=>(
+
+
+      <SwiperSlide className='relative z-20'>
+        <div >
+        <div className="absolute top-0 left-0 bg-blend-darken z-10 w-full h-full bg-[#0a0a0a47]"></div>
+        <div className="absolute ml-2 mt-2 top-2 left-0 p-4 rounded-lg text-white font-semibold bg-[#0a0a0a80]">
+        <h2 className=" text-3xl">{dest.Name}</h2>
+        <h3>{dest.number}</h3>
+        </div>
+        <img
+              src={dest.image}
+              alt={dest.Name}
+              className="object-cover rounded-lg h-[480px] w-[800px]"
             />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a
-                href={`#slide${index === 0 ? index : index - 1}`}
-                className={`
-                        btn btn-circle 
-                        ${index === 0 ? "opacity-0 cursor-default" : ""}
-                      `}
-              >
-                ❮
-              </a>
-              <a
-                href={`#slide${index === destinations?.length - 1 ? index : index + 1
-                  }`}
-                className={`
-                        btn btn-circle 
-                        ${index === destinations?.length - 1
-                    ? "opacity-0 cursor-default"
-                    : ""
-                  }
-                      `}
-              >
-                ❯
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+        </div>
+            
+      </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
